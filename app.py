12a6cc5cdf28e9e557c4c4a9fcde7a85c56b2f2e
trenyc <trentonglore@yahoo.com>
2024-03-20 14:11:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -105,18 +105,15 @@ st.title("🤖 Google Gemini Comments")
 st.caption("🚀 A streamlit app powered by Google Gemini")
 
 # Create tabs for the Streamlit app
-tab1, tab2 = st.tabs(["🌏 Generate Travel Plans - Gemini Pro", "🖼️ Visual Venture - Gemini Pro Vision"])
+tab1 = st.tabs(["🌏 Generate Funny Comment - Gemini Pro"])
 
 # Code for Gemini Pro model
 with tab1:
     st.write("💬 Using Gemini Pro - Text only model")
     st.subheader("🌏 Generate funniest comment")
     
-    destination_name = st.text_input("Enter destination name: \n\n",key="destination_name",value="United Arab Emirates")
-    days = st.text_input("How many days would you like the itinerary to be? \n\n",key="days",value="5")
-    suggested_attraction = st.text_input("What should the first suggested attraction be for the trip? \n\n",key="suggested_attraction",value="Visiting Burj Khalifa in Dubai.")
 
-    prompt = f"""Come up with a {days}-day itinerary for a trip to {destination_name}. The first suggested attraction should be {suggested_attraction}
+    prompt = f"""Come up with a funny comment 
        """ 
     
     config = {
@@ -124,43 +121,16 @@ with tab1:
         "max_output_tokens": 2048,
         }
     
-    generate_t2t = st.button("Generate my travel itinerary", key="generate_t2t")
+    generate_t2t = st.button("Generate funny comment", key="generate_t2t")
     model = genai.GenerativeModel("gemini-pro", generation_config=config)
     if generate_t2t and prompt:
-        with st.spinner("Generating your travel itinerary using Gemini..."):
-            plan_tab, prompt_tab = st.tabs(["Travel Itinerary", "Prompt"])
+        with st.spinner("Generating your funny comment using Gemini..."):
+            plan_tab, prompt_tab = st.tabs(["Funny Comment", "Prompt"])
             with plan_tab: 
                 response = model.generate_content(prompt)
                 if response:
-                    st.write("Your plan:")
+                    st.write("Your comment:")
                     st.write(response.text)
             with prompt_tab: 
                 st.text(prompt)
 
-# Code for Gemini Pro Vision model
-with tab2:
-    st.write("🖼️ Using Gemini Pro Vision - Multimodal model")
-    st.subheader("🔮 Generate image to text responses")
-    
-    image_prompt = st.text_input("Ask any question about the image", placeholder="Prompt", label_visibility="visible", key="image_prompt")
-    uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
-    image = ""
-
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image.", use_column_width=True)
-
-    submit=st.button("Generate Response")
-
-    if submit:
-        model = genai.GenerativeModel('gemini-pro-vision')
-        with st.spinner("Generating your response using Gemini..."):
-            if image_prompt!="":
-                response = model.generate_content([image_prompt,image])
-            else:
-                response = model.generate_content(image)
-        response = response.text
-        st.subheader("Gemini's response")
-        st.write(response)
-
-    
