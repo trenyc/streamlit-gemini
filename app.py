5f351e4dc28e38d5d5f2bf7727722331579da2fa
trenyc <trentonglore@yahoo.com>
2024-03-20 13:58:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ with st.sidebar:
             for item in response['items']:
                 yttext = yttext + item['snippet']['topLevelComment']['snippet']['textDisplay']             
                 print(item['snippet']['topLevelComment']['snippet']['textDisplay'])
-            st.text_area(" ")
+            st.text_area('{yttext}')
             st.write('{yttext}')
 
           
