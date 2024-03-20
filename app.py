c4a22ab1658e440ff6fe9c0a1063fe23dd7d76b7
trenyc <trentonglore@yahoo.com>
2024-03-20 13:19:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,25 @@ with st.sidebar:
         if not (yt_api_key.startswith('AI')):
             st.warning('Please enter your API Key!', icon='⚠️')
         else:
+            yt_api_key = st.secrets['YOUTUBE_API_KEY']
+            api_service_name = "youtube"
+            api_version = "v3"
+            DEVELOPER_KEY=yt_api_key
+              
+            youtube = googleapiclient.discovery.build(
+                api_service_name, api_version, developerKey=DEVELOPER_KEY)
+              
+            request = youtube.commentThreads().list(
+                part="snippet",
+                videoId="WNrB1Q9Rry0",
+                maxResults=100
+              )
+            response = request.execute()
+            for item in response['items']:
+                st.write(item['snippet']['topLevelComment']['snippet']['textDisplay'])
+                print(item['snippet']['topLevelComment']['snippet']['textDisplay'])
             st.success('Success!', icon='✅')
+
           
 
 
