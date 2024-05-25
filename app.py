bf7e3c61fc8f34f708d7e8ff2de5b583c1eae8ab
trenyc <trentonglore@yahoo.com>
2024-05-25 06:54:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -281,7 +281,8 @@ def create_vote_button(video_id, comment_id, category, vote_type="up"):
     button_key = f"{category}_{vote_type}_{comment_id}_{str(uuid.uuid4())}"
 
     if st.button(button_text, key=button_key):
-        update_votes(video_id, comment_id, category, vote_type)
+        st.write("test")
+        #update_votes(video_id, comment_id, category, vote_type)
         #st.experimental_rerun()  # Force rerun to update vote count
 
 # Function to display categorized comments
