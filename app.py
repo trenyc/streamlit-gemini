7ec64994eb1a45d5f780aa16d086042c4e3f80a7
trenyc <trentonglore@yahoo.com>
2024-05-25 22:17:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -381,7 +381,7 @@ try:
     if 'categorized_comments' in st.session_state and any(st.session_state.categorized_comments.values()) and not st.session_state.load_more_clicked:
         display_categorized_comments(prevent_votes=True)
 except Exception as e:
-    #st.error(f"An unexpected error occurred: {e}")
+    st.error(f"An unexpected error occurred: {e}")
 
 
 # Load more comments button
